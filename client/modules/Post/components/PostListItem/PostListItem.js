2b1d2a61cc9defc57d940f8189807586551bab7f
Patryk Mazurkiewicz <patrykiewicz@gmail.com>
2017-04-11 20:31:43 +0200

    voting on posts list

--- client/modules/Post/components/PostListItem/PostListItem.js
+++ client/modules/Post/components/PostListItem/PostListItem.js
@@ -1,9 +1,12 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 import { FormattedMessage } from 'react-intl';
+import { connect } from 'react-redux';
 
 // Import Style
 import styles from './PostListItem.css';
+
+import { voteUpRequest, voteDownRequest } from '../../PostActions';
 
 function PostListItem(props) {
   return (
@@ -16,6 +19,11 @@ function PostListItem(props) {
       <p className={styles['author-name']}><FormattedMessage id="by" /> {props.post.name}</p>
       <p className={styles['post-desc']}>{props.post.content}</p>
       <p className={styles['post-action']}><a href="#" onClick={props.onDelete}><FormattedMessage id="deletePost" /></a></p>
+      <p>
+        <span className={styles['post-vote']} onClick={() => props.voteDownRequest(props.post.cuid, props.post.voteCount)}>-</span>
+        {props.post.voteCount}
+        <span className={styles['post-vote']} onClick={() => props.voteUpRequest(props.post.cuid, props.post.voteCount)}>+</span>
+      </p>
       <hr className={styles.divider} />
     </div>
   );
@@ -32,4 +40,11 @@ PostListItem.propTypes = {
   onDelete: PropTypes.func.isRequired,
 };
 
-export default PostListItem;
+function mapDispatchToProps(dispatch, props) {
+  return {
+    voteUpRequest: (cuid, votes) => dispatch(voteUpRequest(cuid, votes)),
+    voteDownRequest: (cuid, votes) => dispatch(voteDownRequest(cuid, votes)),
+  };
+}
+
+export default connect(null, mapDispatchToProps)(PostListItem);
